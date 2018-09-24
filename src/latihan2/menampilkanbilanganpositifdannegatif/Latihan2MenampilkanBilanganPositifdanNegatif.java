0ba2e6537cfe6dc1c2a3870427ce211acab99f91
mif99ms <43515183+mif99ms@users.noreply.github.com>
2018-09-25 06:31:27 +0700

    Initial commit

---
+++ src/latihan2/menampilkanbilanganpositifdannegatif/Latihan2MenampilkanBilanganPositifdanNegatif.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2.menampilkanbilanganpositifdannegatif;
+
+/**
+ *
+ * @author USER99
+ */
+public class Latihan2MenampilkanBilanganPositifdanNegatif {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      double nBilangan;
+      
+      nBilangan = 10.59;
+     
+        System.out.println(nBilangan + "adalah bilangan positif" n );
+      
+    }
+    
+}
